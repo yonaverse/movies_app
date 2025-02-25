76fc89d877f0a9a8cdd6d32d2ae40c4f0d3d4672
yonaverse <yon4sdev@outlook.com>
2025-02-25 14:25:13 +0300

    implement movie fetching with error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,60 @@
-import { useState } from "react";
+import detenv from "dotenv";
+import { useState, useEffect } from "react";
 import Search from "./component/search";
+import Spinner from "./component/spinner";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.TMDB;
+
+// const URL =
+//   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer${API_KEY}`,
+  },
+};
 
 const App = () => {
   const [searchterm, setSearchterm] = useState("");
+  const [errormessage, setErrormessage] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState("flase");
+
+  const fetchMovies = async () => {
+    setLoading(true);
+    setErrormessage("");
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("An error occured. Please try again later.");
+      }
+
+      const data = await response.json();
+
+      if (data.response === false) {
+        setErrormessage("An error occured. Please try again later.");
+        setMovies([]);
+        return;
+      }
+
+      setMovies(data.results || []);
+    } catch (error) {
+      console.error(`error: ${error}`);
+      setErrormessage("An error occured. Please try again later.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <main className="bg-[image:url('/assets/BG.png')]">
@@ -19,8 +71,26 @@ const App = () => {
               find <span className="text-gradient">movies</span> you'll enjoy
               without a hassle
             </h1>
+            <Search searchterm={searchterm} setSearchterm={setSearchterm} />
           </header>
-          <Search searchterm={searchterm} setSearchterm={setSearchterm} />
+
+          <section className="all-movies">
+            <h2 className="mt-[40px]">All movies</h2>
+
+            {loading ? (
+              <Spinner />
+            ) : errormessage ? (
+              <p className="text-red-500">{errormessage}</p>
+            ) : (
+              <ul>
+                {movies.map((movie) => (
+                  <p className="text-white" key={movie.id}>
+                    {movie.title}
+                  </p>
+                ))}
+              </ul>
+            )}
+          </section>
         </div>
       </div>
     </main>
